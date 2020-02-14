0e8c1994ada69187ed612efddb8b544c5ec22b93
Roy Springer <r.springer@rocmondriaan.nl>
2020-02-14 15:16:42 +0100

    Add post login route

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser);
 
 let mockUserData = [
     {name: "Roy"},
@@ -12,6 +14,36 @@ app.get("/users", (req, res) => {
         message: "Succesfully got users",
         users: mockUserData
     });
+});
+
+app.get("/user/:id", (req, res) => {
+    console.log(req.params.id);
+    res.json({
+        success: true,
+        message: "Succesfully got one user",
+        user: req.params.id
+    });
+});
+
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+ 	const password = req.body.password;
+ 
+ 	const mockUsername = "billyTheKid";
+ 	const mockPassword = "superSecret";
+ 
+ 	if (username === mockUsername && password === mockPassword){
+      	res.json({
+      	 	success: true,
+      	 	message: 'password and username match!',
+      	 	token: 'encrypted token goes here'
+      	})
+ 	} else {
+      	res.json({
+      	 	success: false,
+      	 	message: 'password and username do not match'
+      	})
+ 	}
 });
 
 app.listen(8000, () => {
