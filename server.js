34de5514b45c9e58e4b23a688835e6300fdd129c
Roy Springer <r.springer@rocmondriaan.nl>
2020-02-14 13:57:12 +0100

    First GET route

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 
+let mockUserData = [
+    {name: "Roy"},
+    {name: "Pascal"},
+]
+
+app.get("/users", (req, res) => {
+    res.json({
+        success: true,
+        message: "Succesfully got users",
+        users: mockUserData
+    });
+});
+
 app.listen(8000, () => {
     console.log("Server started on port 8000");
 })
